de2be87de346a319e0ce9f0b2834360e583a7163
Bashar <basharsaeed200@gmail.com>
2023-06-12 23:45:46 +0000

    worked on admin approval for image update

--- StudentID/Controllers/AdminController.cs
+++ StudentID/Controllers/AdminController.cs
@@ -26,7 +26,24 @@ namespace StudentID.Controllers
 						where st != null
 						select new { Student = s, IDCard = c, MStud = st };
 
-			return View(query.ToList());
+			var query1 = from s in _db.Students
+						 join i in _db.ImageUpdates on s.Id equals i.StudentId into stud_mod
+						 from st in stud_mod.DefaultIfEmpty()
+						 where st != null
+						 select new { 
+							 currentImg=s.ImageUrl, 
+							 newImg=st.FileName, 
+							 requestId=st.Id, 
+							 studentId=st.StudentId
+						 };
+
+			var viewState = new
+			{
+				nameModifications = query.ToList(),
+				imageUpdates = query1.ToList()
+			};
+
+			return View(viewState);
 		}
 
 		public IActionResult GetNameModificationDetail(Guid Id)
@@ -73,8 +90,8 @@ namespace StudentID.Controllers
 				}
 				else
 				{
-					return Json(new { status = false, msg = "Error Deleting File!!" });
 					Console.WriteLine("File not found");
+					return Json(new { status = false, msg = "Error Deleting File!!" });
 				}
 				_db.NameModificationDocuments.Remove(nModify);
 
@@ -90,10 +107,136 @@ namespace StudentID.Controllers
 		}
 
 		[HttpPost]
-		public IActionResult RejectNameModificationRequest([FromBody] NameModificationApproval reqBody)
+		public async Task<IActionResult> RejectNameModificationRequest([FromBody] NameModificationApproval reqBody)
 		{
-			
-			return Json(reqBody);
+			try
+			{
+				var nModify = _db.NameModificationDocuments
+					.FirstOrDefault(
+						x => x.Id == Guid.Parse(reqBody.RequestId) &&
+							 x.StudentId == Guid.Parse(reqBody.StudentId)
+					);
+
+				if (nModify == null)
+				{
+					throw new Exception("Record Was Not Found");
+				}
+
+				string wwwRootPath = _hostEnv.WebRootPath;
+				string fname = nModify.FileName;
+				string filePath = Path.Combine(wwwRootPath + "/image/ProofOfNames", fname);
+				if (System.IO.File.Exists(filePath))
+				{
+					// Delete the file
+					System.IO.File.Delete(filePath);
+					Console.WriteLine("File removed successfully");
+				}
+				else
+				{
+					Console.WriteLine("File not found");
+					return Json(new { status = false, msg = "Error Deleting File!!" });
+				}
+				_db.NameModificationDocuments.Remove(nModify);
+
+				await _db.SaveChangesAsync();
+
+				return Json(new { status = true, msg = "Name Modification Rejected Successfully!!" });
+			}
+			catch (Exception ex)
+			{
+				return Json(new { status = false, msg = "Unexpected Error!!" });
+			}
+
+		}
+	
+		[HttpPost]
+		public async Task<IActionResult> ApproveImageUpdate([FromBody] NameModificationApproval reqBody)
+		{
+			try
+			{
+				var student = _db.Students.FirstOrDefault(x => x.Id == Guid.Parse(reqBody.StudentId));
+				var nModify = _db.ImageUpdates
+					.FirstOrDefault(
+						x => x.Id == Guid.Parse(reqBody.RequestId) &&
+							 x.StudentId == Guid.Parse(reqBody.StudentId)
+					);
+
+				if (student == null || nModify == null)
+				{
+					throw new Exception("Record Was Not Found");
+				}
+
+
+				string wwwRootPath = _hostEnv.WebRootPath;
+				string fname = nModify.FileName;
+				string oldPath = Path.Combine(wwwRootPath + "/image/approved", student.ImageUrl);
+				string newPath = Path.Combine(wwwRootPath + "/image/updates", nModify.FileName);
+				if (System.IO.File.Exists(oldPath))
+				{
+					// Delete the file
+					System.IO.File.Delete(oldPath);
+					System.IO.File.Move(newPath, Path.Combine(wwwRootPath + "/image/approved", nModify.FileName));
+					student.ImageUrl = nModify.FileName;
+					await _db.SaveChangesAsync();
+					Console.WriteLine("File removed successfully");
+				}
+				else
+				{
+					Console.WriteLine("File not found");
+					return Json(new { status = false, msg = "Error Deleting File!!" });
+				}
+				return Json(new { status = true, msg = "Image Update Approved Successfully!!" });
+			}
+			catch (Exception ex)
+			{
+				return Json(new { status = false, msg = "Unexpected Error!!" });
+			}
+
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> RejectImageUpdate([FromBody] NameModificationApproval reqBody)
+		{
+			try
+			{
+				var student = _db.Students.FirstOrDefault(x => x.Id == Guid.Parse(reqBody.StudentId));
+				var nModify = _db.ImageUpdates
+					.FirstOrDefault(
+						x => x.Id == Guid.Parse(reqBody.RequestId) &&
+							 x.StudentId == Guid.Parse(reqBody.StudentId)
+					);
+
+				if (student == null || nModify == null)
+				{
+					throw new Exception("Record Was Not Found");
+				}
+
+
+				string wwwRootPath = _hostEnv.WebRootPath;
+				string fname = nModify.FileName;
+				// string oldPath = Path.Combine(wwwRootPath + "/image/approved", student.ImageUrl);
+				string newPath = Path.Combine(wwwRootPath + "/image/updates", nModify.FileName);
+				if (System.IO.File.Exists(newPath))
+				{
+					// Delete the file
+					System.IO.File.Delete(newPath);
+					// System.IO.File.Move(newPath, Path.Combine(wwwRootPath + "/image/approved", nModify.FileName));
+					_db.ImageUpdates.Remove(nModify);
+					await _db.SaveChangesAsync();
+					Console.WriteLine("File removed successfully");
+				}
+				else
+				{
+					Console.WriteLine("File not found");
+					return Json(new { status = false, msg = "Error Deleting File!!" });
+				}
+				return Json(new { status = true, msg = "Image Update Rejected Successfully!!" });
+			}
+			catch (Exception ex)
+			{
+				return Json(new { status = false, msg = "Unexpected Error!!" });
+			}
+
 		}
 	}
 }
